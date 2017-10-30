cc7fc3bac581b098a847543bbf3b39dfa5bfdbe6
Onur Eker <yonureker@gmail.com>
2017-10-30 18:34:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   if (string = string.toLowerCase()){
     return("I can't hear you!");
   }
-    elseif (string = string.toUpperCase()){
+    else if (string = string.toUpperCase()){
       return("YES INDEED!");
   }
     else

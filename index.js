e96d6bdee1d45eb84d9eb59b1f0c11dd8c16513c
Onur Eker <yonureker@gmail.com>
2017-10-30 18:32:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ function logShout(string){
   console.log(string.toUpperCase())
 }
 
-function sayHiToGrandma{
-  if string = string.toLowerCase()
-    return("I can't hear you!")
-  elseif string = string.toUpperCase()
-    return("YES INDEED!")
+function sayHiToGrandma(string){
+  if (string = string.toLowerCase()){
+    return("I can't hear you!");
+  }
+  elseif (string = string.toUpperCase()){
+    return("YES INDEED!");
+  }
   else
-    return("I love you, too.")
-  )
+    {return("I love you, too.");
+  }
 }
